bd8621759e19ecd0aa25f4ba0b91a135a9dea7fb
Chris Wang <ciwang618@gmail.com>
2026-02-08 15:18:34 -0500

    Removed unnecessary libraries from main, updated README

--- MainBlynkServo.ino
+++ MainBlynkServo.ino
@@ -1,104 +1,41 @@
 // this code works with blynk with the uno r4, can read what is being printed on serial monitor 
 
-
-// Paste in your Blynk information from the dashboard
+// Paste in your Blynk information from the website dashboard
 #define BLYNK_TEMPLATE_ID "TMPL2tSBE42IL"
 #define BLYNK_TEMPLATE_NAME "Rockblock Terminal"
 #define BLYNK_AUTH_TOKEN "Y6jcLMKWOxE5FCyg30M7h6l0XzSPwfuR"
-#include <Wire.h>              //OLED/LUX
-#include <SPI.h>               //SD/ATM
-#include <SD.h>                //SD
-#include <TinyGPS++.h>         //GPS          TinyGPSPlus by Mikal Hart
-#include <Adafruit_SSD1306.h>  //OLED         Adafruit SSD1306 by Adafruit
-#include <Adafruit_Sensor.h>   //ATM/LUX
-#include <Adafruit_BME680.h>   //ATM          Adafruit BME680 Library by Adafruit
-#include "Adafruit_TSL2591.h"  //LUX          Adafruit TSL2591 Library by Adafruit
-#include "Adafruit_SHT31.h"    // TEMP/HU     Adafruit SHT31 Library by Adafruit
 
 #include <Servo.h>
-
-#include <IridiumSBD.h>        //             Iridium SBD by Mikal Hart
+#include <IridiumSBD.h>
 #include <WiFiS3.h>
 #include <BlynkSimpleWifi.h>
 
-
-
-#define DIAGNOSTICS false  // Change this to see diagnostics
-
-//=====================================
-//ATM =================================
-#define SEALEVELPRESSURE_HPA (1013.2)
-#define BME_SCK 40
-#define BME_MISO 41
-#define BME_MOSI 42
-#define BME_CS 43
+#define DIAGNOSTICS false  // Change to true to see diagnostics
 #define seconds() (millis() / 1000)
 
-Adafruit_BME680 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK);
-
+// Rockblock variables 
 #define IridiumSerial Serial1 // The arduino mega has multiple uart ports (tx/rx1-3), the uno r4 only has Serial1
-
-//Rockblock variables =======================
 IridiumSBD iridium(IridiumSerial);
 
 uint8_t buffer[200] = { 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89 };
-
-int buzzerPin = 11;
-
-int sq;
-
-bool sendSuccess;
-
-bool cut;
-
 uint8_t bufferSend[270];
 size_t bufferSizeSend = 0;
 
-//=====================================
+int buzzerPin = 11;
 
-//ATM variables =======================
-float pascals;
-float altm;
-float humidity;
-float gas;
-float tempC;
+int sq; // Signal quality
+static bool messageSent = false;
 
-//=====================================
-//LUX =================================
-Adafruit_TSL2591 tsl = Adafruit_TSL2591(2591);
+bool cut;
 
-//LUX variables =======================
-uint32_t lum;
-int16_t ir, full;
-float lux;
-
-TinyGPSPlus gps;
-double latit;   //degrees
-double longit;  //degrees
-double alt;     //meters
-double alto;
-double velDiff;
-double vel[] = { 0, 0, 0, 0, 0 };  //meters
-long gpstime;                      //HHMMSSCC
-long gpsdate;                      //DDMMYY
-int numsats;
-double course;  //degrees
-double speed;   //mph
-
-//SD Card ==================================
-#define chipSelect 53
-
-//TEMP/HU ==================================
-Adafruit_SHT31 sht31 = Adafruit_SHT31();
-double sht31_temp;
-double sht31_humidity;
-
-//Servo ====================================
+//Servo
 Servo myServo;
 
-char ssid[] = "iPhone Chris Wang"; // if using an iOS hotspot, turn on "Maximize Compatibility"
-char pass[] = "crtqveus";
+// WiFi ssid and password
+char ssid[] = ""; // if using an iOS hotspot, turn on "Maximize Compatibility"
+char pass[] = "";
 
+// Writing from the blynk to Arduino, for testing the cutter
 BLYNK_WRITE(V0) {
   String receivedString = param.asString();
     
@@ -116,19 +53,15 @@ BLYNK_WRITE(V0) {
 }
 
 void setup() {
-  //Serial
-  Serial.begin(115200);                       //Computer
-
+  Serial.begin(115200);                       // Computer serial
   Serial.println("Test"); 
-  //Serial1.begin(115200);                      //cutter uart, change wired connection
-  //Serial2.begin(9600);                        //GPS uart
 
   myServo.attach(3);                          
-  myServo.write(0);                           //starting servo position
+  myServo.write(0);                           // starting servo position
 
-  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);  //blynk terminal
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);  // blynk terminal setup, further code will not run until connection is made
 
-  //rockblock
+  // Rockblock setup
   pinMode(buzzerPin, OUTPUT);
   while (!Serial);
   IridiumSerial.begin(19200);
@@ -142,11 +75,8 @@ void setup() {
     Blynk.virtualWrite(V0, "Couldn't begin modem operations.");
     exit(0);
   }
-  //rockblock
   cut = false;
 }
-
-static bool messageSent = false;
 
 void loop() {
   
@@ -154,35 +84,17 @@ void loop() {
 
   if (seconds() > 5400 && cut == false) {  // cutdown after 90min
     for (int i = 0; i < 5; i++) {
-      //Serial1.write("!Acc");  // serial1 is 19 (RX1), 18 (TX1), REPLACED WITH Iridium
       myServo.write(90); 
       delay(3000);
-      //Serial1.write("!Aco");
       delay(3000);
     }
-    //Serial1.write("!Acc");
     cut = true;
   }
-  // String data = mkdata();
-  // Serial.println(data);
-  // writeSD(data);
-  // delay(100);
-
-  // String dataSend = mkdataSend();
-  // char outBuffer[270];
-  // for (int i = 0; i < dataSend.length(); i++) {
-  //   outBuffer[i] = dataSend[i];
-  // }
-
-  // uint8_t rxBuffer[270] = {0};
-  // for (int i = 0; i < dataSend.length(); i++) {
-  //   rxBuffer[i] = outBuffer[i];
-  // }
 
   bufferSizeSend = sizeof(bufferSend);
 
   int signalQuality = -1;
-  // Check the signal quality
+  // Check the signal quality (ranging from 1 to 5), return it if connection is made
   int errS = iridium.getSignalQuality(signalQuality);
   if (errS != 0) {
     Serial.print("SignalQuality failed: error ");
@@ -197,18 +109,13 @@ void loop() {
   Blynk.virtualWrite(V0, signalQuality);
   sq = signalQuality;
 
-  //indentation?????
-  // Control the active buzzer to beep 'x' times in a group
-
+  // more beeps signify better signal quality
   int pitches[] = { 440, 667, 880, 1333, 1011, 880 };
-
   for (int j = 0; j < 3; j++) {
     for (int i = 0; i < sq + 1; i++) {
-      // Beep (generate a tone) for a specific duration
       beep(pitches[i], 500);
       delay(100);
     }
-    // Add a longer delay between groups if needed
   }
 
   if (sq > 0) {
@@ -236,6 +143,7 @@ void loop() {
         beep(506, 250);
         beep(440, 250);
         beep(383, 250);
+
         Serial.print("Timeout after 30 seconds...Probably due to bad signal quality. Error code: ");
         Blynk.virtualWrite(V0, "Timeout after 30 seconds...Probably due to bad signal quality. Error code: ");
         Serial.println(err);
@@ -245,8 +153,7 @@ void loop() {
       } else if (iridium.getWaitingMessageCount() == 0) {
         Serial.println("No Messages to Display.");
         Blynk.virtualWrite(V0, "No Messages to Display.");
-      } else  // success!
-      {
+      } else {
         messageSent = true;
         Serial.println();
         Serial.print("Inbound buffer size is ");
@@ -270,8 +177,7 @@ void loop() {
         Blynk.virtualWrite(V0, "The full message received is: ");
         Serial.println(mes);
         Blynk.virtualWrite(V0, mes);
-        //       //change corresponding wiring
-        //Serial1.write(mes); NOTHING IS BEING WRITTEN TO THE CUTTER
+        
         beep(300, 500);
         beep(400, 500);
         beep(500, 500);
@@ -280,9 +186,10 @@ void loop() {
         Serial.println();
         Serial.print("Messages sent to cutter!");
 
-        myServo.write(180); //testing purposes
+        // servo control, change the write calls to properly cutdown
+        myServo.write(180); 
         if ((String(mes)).equals("!AccoQ")) {
-          myServo.write(75);                                             //turn servo
+          myServo.write(75);                                             
         } else if ((String(mes)).equals("!AcooQ")) {
           myServo.write(0);
         }
@@ -293,30 +200,12 @@ void loop() {
         Blynk.virtualWrite(V0, "Messages remaining to be retrieved: ");
         Serial.println(iridium.getWaitingMessageCount());
         Blynk.virtualWrite(V0, iridium.getWaitingMessageCount());
-
-        // int senderr;
-        // Serial.println("Sending data to control...");
-        // beep(1000,2000);
-        // senderr = modem.sendReceiveSBDBinary(rxBuffer, dataSend.length(), bufferSend, bufferSizeSend);
-        // if (senderr != ISBD_SUCCESS)
-        // {
-        //        beep(3700, 1000);
-        //        beep(3100, 1000);
-        //        beep(2500, 1000);
-        //        Serial.print("Unable to send data to control after 30 seconds...Probably due to bad signal quality. Error code: ");
-        //        Serial.println(senderr);
-        //        sendSuccess = false;
-        //  }
-        //  else{
-        //    Serial.print("Message successfully sent to control is: ");
-        //    Serial.println(dataSend);
-        //    sendSuccess = true;
-        //  }
       }
     }
   }
 }
-// Function to generate a beep with a specific frequency and duration
+
+// Generate a beep with a specific frequency and duration
 void beep(int frequency, int duration) {
   tone(buzzerPin, frequency);
   delay(duration);
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# sp26-cutdown
+# Spring 2026 Arduino Cutdown
+
+The software runs on a Arduino UNO R4 and Rockblock 9603
+
+# Libraries
+ - Servo
+ - IridiumSBD by Mikal Hart
+ - WiFiS3
+ - BlynkSimpleWifi
+
+# Setup
+1. Create Blynk device with terminal (virtual pin input)
+2. Put ID and auth token into the define fields.
+3. Put WiFi/hotspot ssid and password into the variables
+
+Note: The device not execute any code until connected to Blynk, but once connected will run even after being disconnected
+
+# Cutdown
+ - Cutdown just repeatedly actuates the servo to release payload
+ - Cutdown automatically occurs after 90 minutes if not already activated
+ - Can manually cutdown with !Acc and !Aco commands either from Blynk (for testing) or RockBLOCK
